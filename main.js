ccd4a20b6bfcb35e6dd0204f95aadd301150945e
Sandra Soueid <sfsoueid@gmail.com>
2024-09-17 13:46:03 -0400

    Secure methods

--- main.js
+++ main.js
@@ -1,52 +1,51 @@
 // main.js
 
-import { app, BrowserWindow, ipcMain } from 'electron';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { app, BrowserWindow, ipcMain } from "electron";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 // Fix __dirname and __filename in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-import { JSONFilePreset } from 'lowdb/node';
+import { JSONFilePreset } from "lowdb/node";
 
 // LowDB setup
 const defaultData = { tasks: {} };
-const db = await JSONFilePreset(path.join(__dirname, 'db.json'), defaultData);
+const db = await JSONFilePreset(path.join(__dirname, "db.json"), defaultData);
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      enableRemoteModule: false,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  win.loadFile('index.html');
+  win.loadFile("index.html");
 }
 
-ipcMain.handle('please', (...args) => {
-    console.log('Sent args:', args)
-    return 'Thanks!'
-  })
-
-ipcMain.handle('get-tasks', async () => {
-  return await db.read();
-});
-
-ipcMain.handle('set-tasks', async (event, tasks) => {
-  await db.write({ tasks });
+ipcMain.on("toMain", (event, data) => {
+  // Handle incoming data from renderer process
+  console.log("hello", data);
+  win.webContents.send("fromMain", "Hello from main process!");
 });
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
--- preload.js
+++ preload.js
@@ -1,5 +1,25 @@
-// preload.js
+const {
+  contextBridge,
+  ipcRenderer
+} = require("electron");
 
-window.addEventListener("DOMContentLoaded", () => {
-  // Preload scripts can expose APIs to the renderer process
-});
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+  "api", {
+      send: (channel, data) => {
+          // whitelist channels
+          let validChannels = ["toMain"];
+          if (validChannels.includes(channel)) {
+              ipcRenderer.send(channel, data);
+          }
+      },
+      receive: (channel, func) => {
+          let validChannels = ["fromMain"];
+          if (validChannels.includes(channel)) {
+              // Deliberately strip event as it includes `sender` 
+              ipcRenderer.on(channel, (event, ...args) => func(...args));
+          }
+      }
+  }
+);
